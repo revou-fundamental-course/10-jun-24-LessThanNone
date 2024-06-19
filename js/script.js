5c4db9ab6fc2eb9af38a8c31984cdea37c2a3c26
LessThanNone <charless.23662020@gmail.com>
2024-06-19 22:29:16 +0700

    update script

--- js/script.js
+++ js/script.js
@@ -24,11 +24,12 @@ convertButton.addEventListener('click', function() {
   if (!isNaN(celciusValue)) {
     const fahrenheitValue = celciusToFahrenheit(celciusValue);
     output.value = fahrenheitValue.toFixed(2);
-    calculateOutput.innerHTML = `${input.value}&deg;C × 9/5) + 32 = ${output.value}&deg;F`;
+    calculateOutput.innerHTML = `${input.value}&deg;C × (9/5) + 32 = ${output.value}&deg;F`;
   } else {
-    alert('Please enter a valid Celcius value');
+    alert('Tolong masukkan derajat suhunya!');
   }
 });
+
 
 // reset button
 resetButton.addEventListener('click', function() {
@@ -36,3 +37,34 @@ resetButton.addEventListener('click', function() {
   output.value = '';
   calculateOutput.innerHTML = '';
 });
+
+// Reverse button 
+let isReversed = false;
+reverseButton.addEventListener('click', function() {
+  isReversed = !isReversed;
+  if (isReversed) {
+    celciusText.innerHTML = 'Fahrenheit (&deg;F): ';
+    fahrenheitText.innerHTML = 'Celcius (&deg;C): ' ;
+    output.value = '';
+    calculateOutput.innerHTML = '';
+  } else {
+    celciusText.innerHTML = 'Celcius (&deg;C): ';
+    fahrenheitText.innerHTML = 'Fahrenheit (&deg;F): ' ;
+    output.value = '';
+    calculateOutput.innerHTML = '';
+  }
+});
+
+  // convert fahrenheit ke celcius
+  convertButton.addEventListener('click', function() {
+    if (isReversed) {
+      const fahrenheitValue = parseFloat(input.value);
+      if (!isNaN(fahrenheitValue)) {
+        const celciusValue = fahrenheitToCelcius(fahrenheitValue);
+        output.value = celciusValue.toFixed(2);
+        calculateOutput.innerHTML = `${input.value}&deg;F × 5/9 - 32 = ${output.value}&deg;C`;
+      } else {
+        alert('Tolong masukkan derajat suhunya!');
+      }
+    }
+  });
